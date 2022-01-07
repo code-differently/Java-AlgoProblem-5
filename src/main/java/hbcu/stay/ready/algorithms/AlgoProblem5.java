ac31f21afcd283d53a016b8e27f9f76c97f996a7
kalebCodeDiff <=>
2022-01-07 16:33:16 -0500

    v2

--- src/main/java/hbcu/stay/ready/algorithms/AlgoProblem5.java
+++ src/main/java/hbcu/stay/ready/algorithms/AlgoProblem5.java
@@ -4,16 +4,7 @@ public class AlgoProblem5 {
 
     public String greatEncrypt(String message, int shift){
 
-        String response = "";
-
-        for(char c : message.toCharArray()){
-            if(Character.isLetter(c)) {
-                c += shift;
-            }
-            response += c;
-        }
-
-        return response;
+        return null;
 
     }
 }
